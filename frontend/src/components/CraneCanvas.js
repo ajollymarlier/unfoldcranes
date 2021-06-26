8cb1a14ecf1f297918f507dde9bac3b0f7a5dfc8
Arun Jolly Marlier <arunjm00@gmail.com>
2021-06-26 10:34:13 -0400

    Added empty string rendering for crane columns

--- frontend/src/components/CraneCanvas.js
+++ frontend/src/components/CraneCanvas.js
@@ -28,11 +28,11 @@ const getCraneNumber = (craneName) => {
 }
 
 //Adds click listener to each image of a crane
-const addCraneClickListeners = (setOpen, currentCranes, setCurrentDisplayedInfo) => {
+const addCraneClickListeners = (setOpen, currentCranes, setCurrentDisplayedInfo, setSeenCranes) => {
     const midImages = document.querySelectorAll('.midStringImg')
     for(let i = 0; i < midImages.length; i++){
         midImages[i].addEventListener('click', (e) => {
-            //TODO append currentCranes[getCraneNumber(e.currentTarget.id) - 1] to seenCranes  
+            setSeenCranes(currentCranes[getCraneNumber(e.currentTarget.id) - 1])
 
             setCurrentDisplayedInfo({
                 message: currentCranes[getCraneNumber(e.currentTarget.id) - 1].message,
@@ -48,7 +48,7 @@ const addCraneClickListeners = (setOpen, currentCranes, setCurrentDisplayedInfo)
     const endImages = document.querySelectorAll('.endStringImg')
     for(let i = 0; i < endImages.length; i++){
         endImages[i].addEventListener('click', (e) => {
-            //TODO append currentCranes[getCraneNumber(e.currentTarget.id) - 1] to seenCranes  
+            setSeenCranes(currentCranes[getCraneNumber(e.currentTarget.id) - 1])
 
             setCurrentDisplayedInfo({
                 message: currentCranes[getCraneNumber(e.currentTarget.id) - 1].message,
@@ -74,14 +74,6 @@ const getCraneCount = async () => {
     return resBody.numCranes
 }
 
-const checkStringTopRender = (currentCranes, min) => {
-    if (currentCranes.length > min){
-        return <img className="emptyStringImg" src="empty_string.png" alt=""/>
-    }
-
-    return
-}
-
 const filterCountryCranes = async (countryCode) =>{
     const craneCountRes = await fetch(
         '/cranes/' + countryCode,
@@ -94,6 +86,22 @@ const filterCountryCranes = async (countryCode) =>{
 
     const resBody = await craneCountRes.json()
     return resBody
+}
+
+const checkEmptyStringRender = (currentCranes, min, max) => {
+    let emptyStrings = []
+    let splitListLength = currentCranes.slice(min, max).length
+
+    for (let i = 0; i < 5 - splitListLength - 1; i++){
+        emptyStrings.push(<img className="emptyStringImg" src="mid_empty_string.png"/>)
+    }
+
+    if (5 - splitListLength != 0){
+        emptyStrings.push(<img className="emptyStringImg" src="end_empty_string.png"/>)
+    }
+
+    console.log(emptyStrings)
+    return emptyStrings
 }
 
 const CraneCanvas = () => {
@@ -113,7 +121,7 @@ const CraneCanvas = () => {
             const getCranesRes = await fetch('/cranes/' + currentCountryCode, {
                 method: 'PUT',
                 headers: {"content-type": "application/json"},
-                body: JSON.stringify({numCranes: 20, currentCranes: []})
+                body: JSON.stringify({numCranes: 20, currentCranes: seenCranes})
             })
 
             let getCranesList = await getCranesRes.json() 
@@ -121,13 +129,15 @@ const CraneCanvas = () => {
             setCurrentCranes(getCranesList)
             
             addCraneAnimation()
-            addCraneClickListeners(setOpen, getCranesList, setCurrentDisplayedInfo)
+            addCraneClickListeners(setOpen, getCranesList, setCurrentDisplayedInfo, setSeenCranes)
 
             //!Cranes are showing as clicked when they were not
 
             //!Cranes with < 5 in 2nd column on, centers vertically
 
             //!Messages are disappearing???
+
+            //TODO seenCranes not working in backend
 
             //TODO need to have pop up when query returns 0 and block the refresh
 
@@ -188,18 +198,22 @@ const CraneCanvas = () => {
                     alignItems="center"
                     xs={2}
                 >   
-                    {checkStringTopRender(currentCranes, 0)}
+                    <img className="emptyStringImg" src="empty_string.png" alt=""/>
                     {currentCranes.map((crane, i) => {
                         //Stops after getting cranes #0-4
                         if (i >= 5){
                             return <span></span>
                         }
 
-                        if (i === currentCranes.length - 1 || i === 4){
+                        if (i === 4){
                             return <img className="endStringImg" id={"crane" + (i + 1)} alt="" src={crane.backgroundColor + "_end_unread_crane.png"}/>
                         }else{
                             return <img className="midStringImg" id={"crane" + (i + 1)} alt="" src={crane.backgroundColor + "_mid_unread_crane.png"}/>
                         }
+                    })}
+
+                    {checkEmptyStringRender(currentCranes, 0, 5).map((stringImg, i) => {
+                        return stringImg
                     })}
                 </Grid>
 
@@ -212,18 +226,22 @@ const CraneCanvas = () => {
                     alignItems="center"
                     xs={2}
                 >        
-                    {checkStringTopRender(currentCranes, 5)}
+                    <img className="emptyStringImg" src="empty_string.png" alt=""/>
                     {currentCranes.map((crane, i) => {
                         //Stops after getting cranes #5-9
                         if (i <= 4 || i >= 10){
                             return <span></span>
                         }
 
-                        if (i === currentCranes.length - 1 || i === 9){
+                        if (i === 9){
                             return <img className="endStringImg" id={"crane" + (i + 1)} alt="" src={crane.backgroundColor + "_end_unread_crane.png"}/>
                         }else{
                             return <img className="midStringImg" id={"crane" + (i + 1)} alt="" src={crane.backgroundColor + "_mid_unread_crane.png"}/>
                         }
+                    })}
+
+                    {checkEmptyStringRender(currentCranes, 5, 10).map((stringImg, i) => {
+                        return stringImg
                     })}
                 </Grid>
 
@@ -236,18 +254,22 @@ const CraneCanvas = () => {
                     alignItems="center"
                     xs={2}
                 >
-                    {checkStringTopRender(currentCranes, 10)}
+                    <img className="emptyStringImg" src="empty_string.png" alt=""/>
                     {currentCranes.map((crane, i) => {
                         //Stops after getting cranes #10-14
                         if (i <= 9 || i >= 15){
                             return <span></span>
                         }
 
-                        if (i === currentCranes.length - 1 || i === 15){
+                        if (i === 15){
                             return <img className="endStringImg" id={"crane" + (i + 1)} alt="" src={crane.backgroundColor + "_end_unread_crane.png"}/>
                         }else{
                             return <img className="midStringImg" id={"crane" + (i + 1)} alt="" src={crane.backgroundColor + "_mid_unread_crane.png"}/>
                         }
+                    })}
+
+                    {checkEmptyStringRender(currentCranes, 10, 15).map((stringImg, i) => {
+                        return stringImg
                     })}
                 </Grid>
 
@@ -260,18 +282,22 @@ const CraneCanvas = () => {
                     alignItems="center"
                     xs={2}
                 >
-                    {checkStringTopRender(currentCranes, 15)}
+                    <img className="emptyStringImg" src="empty_string.png" alt=""/>
                     {currentCranes.map((crane, i) => {                        
                         //Stops after getting cranes #15-19
                         if (i <= 14){
                             return <span></span>
                         }
 
-                        if (i === currentCranes.length - 1 || i === 19){
+                        if (i === 19){
                             return <img className="endStringImg" id={"crane" + (i + 1)} alt="" src={crane.backgroundColor + "_end_unread_crane.png"}/>
                         }else{
                             return <img className="midStringImg" id={"crane" + (i + 1)} alt="" src={crane.backgroundColor + "_mid_unread_crane.png"}/>
                         }
+                    })}
+
+                    {checkEmptyStringRender(currentCranes, 15, 20).map((stringImg, i) => {
+                        return stringImg
                     })}
                 </Grid>
                 <div id="craneMenuDiv"><CraneMenu id="craneMenu" countryFilter={filterCountryCranes} runAnimation={addCraneAnimation} setCurrentCountryCode={setCurrentCountryCode}/></div>
