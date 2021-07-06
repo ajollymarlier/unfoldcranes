e4eef053df61760f1e7d1329cda06c6bd28386b1
yvonne-yang <yvonne.y.yang@outlook.com>
2021-07-06 16:39:59 -0700

    made footer look nicer

--- frontend/src/components/ContactFooter.js
+++ frontend/src/components/ContactFooter.js
@@ -1,24 +1,38 @@
 import '../styles/ContactFooter.css';
 import React from 'react';
 import { FaFacebookF, FaInstagram } from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi'
+import { RiMailOpenLine } from 'react-icons/ri'
 
 const ContactFooter = () => {
 	return (
 		<div class='footer'>
-			{/* <h2>CONTACT</h2> */}
+      <p class='footer-title'>
+        CONTACT
+      </p>
 			<p class="footer-text">
 				Have any comments or questions? We'd love to hear from you!
-        <table class="icon-list" cellSpacing="10">
-          <tr> 
-            <th><HiOutlineMail/></th>
-            <th>unfoldcranes@gmail.com</th>
+        <table class="icon-list" cellPadding="10">
+          <tr>
+            <th class="less-padding"><RiMailOpenLine/></th>
+            <th>unfoldcranes@gmail.com</th> 
             <th>|</th>
             <th><FaInstagram/></th>
             <th><FaFacebookF/></th>
           </tr>
+          {/* // vertical list - kinda ugly
+          <tr> 
+            <th><RiMailOpenLine/></th>
+            <th>unfoldcranes@gmail.com</th> 
+          </tr>
+          <tr>
+            <th><FaInstagram/></th>
+            <th>unfoldcranes</th>
+          </tr>
+          <tr>
+            <th><FaFacebookF/></th>
+            <th>unfoldcranes</th>
+          </tr> */}
         </table>
-        &copy; 2021 The Unfold Cranes Team
 			</p>
 		</div>
 	);
