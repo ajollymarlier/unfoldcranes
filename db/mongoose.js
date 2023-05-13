2c7eb0e20b4e415dc74ef6eee638c0bc65c586ec
Arun Jolly Marlier <arunjm00@gmail.com>
2023-05-13 16:37:30 -0400

    new db uri

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 "use strict"
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://ajollymarlier:sMdd51YxB0JhqMGR@covidcranescluster.6jxt3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://ajollymarlier:YSuQl7XQqksg7Ee8@cluster0.e6gqhvv.mongodb.net/'
 mongoose.connect(process.env.MONGODB_URI || mongoURI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
 
 module.exports = { mongoose }
